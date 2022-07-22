813e181bc111bffff77648c41a0c86bf6cebca0c
fedecanesa <federicocanesa@gmail.com>
2022-07-22 15:55:53 -0300

    An index is created for the pages.

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './Home/Home';
+import Login from './Login/Login';
+import NotFound from './NotFound/NotFound';
+import Register from './Register/Register';
+
+export {
+    Home,
+    Login,
+    NotFound,
+    Register
+}
