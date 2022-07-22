48b9aa9a16b0504fd2a26d7c8cf559a1443b344b
fedecanesa <federicocanesa@gmail.com>
2022-07-22 11:36:23 -0300

    Adding the axios configuration.

---
+++ src/config/axios.config.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+      baseURL: "https://api-3sxs63jhua-uc.a.run.app/v1",
+});
--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = {
-    API_URL: "https://api-3sxs63jhua-uc.a.run.app/v1/"
-};
-
-export default BASE_URL;
