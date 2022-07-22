4a80c21c18c0c737b20a71e22a8541d63dd6c693
fedecanesa <federicocanesa@gmail.com>
2022-07-22 12:18:44 -0300

    Added logic and styles.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './button.scss';
 
-const Button = props => {
+const Button = ({
+    content,
+    disabled,
+    name,
+    type,
+}) => {
     return (
-        <div>Button</div>
+        <button
+            disabled={disabled}
+            name={name}
+            type={type}
+            className='button'
+        >
+            {content}
+        </button>
     )
 }
 
-Button.propTypes = {}
+Button.propTypes = {
+    content: PropTypes.string,
+    disabled: PropTypes.bool,
+    name: PropTypes.string,
+    type: PropTypes.string,
+}
 
 export default Button
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './input.scss';
 
-const Input = props => {
+const Input = ({
+    type,
+    value,
+    onChange,
+    name,
+    placeholder
+}) => {
     return (
-        <div>Input</div>
+        <input
+            placeholder={placeholder}
+            type={type}
+            value={value}
+            name={name}
+            className='input'
+            onChange={onChange}
+        />
     )
 }
 
-Input.propTypes = {}
+Input.propTypes = {
+    type: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    name: PropTypes.string,
+    placeholder: PropTypes.string,
+}
 
 export default Input;
