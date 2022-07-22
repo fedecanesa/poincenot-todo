a151062f24e49dc7ab34bb1b466e89f77894dbb6
fedecanesa <federicocanesa@gmail.com>
2022-07-22 12:20:04 -0300

    It is completed with the missing components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,30 @@
 import Button from './Button/Button';
+import Error from './Error/Error';
 import Filter from './Filter/Filter';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Input from './Input/Input';
 import Layout from './Layout/Layout';
+import Loader from './Loader/Loader';
 import Menu from './Menu/Menu';
 import Modal from './Modal/Modal';
+import Title from './Title/Title';
 import Todo from './Todo/Todo';
-import TodoList from './Todo/TodoList';
+import TodoList from './TodoList/TodoList';
 import TodoFrom from './TodoForm/TodoForm';
 
 export {
     Button,
+    Error,
     Filter,
     Footer,
     Header,
     Input,
     Layout,
+    Loader,
     Menu,
     Modal,
+    Title,
     Todo,
     TodoList,
     TodoFrom
