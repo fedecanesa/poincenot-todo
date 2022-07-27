dbb381c141a135cd1df105950187cd1574034ee8
fedecanesa <federicocanesa@gmail.com>
2022-07-27 12:35:54 -0300

    The logic of reset and logut is performed and styled

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { logo } from '../../utils/images';
+import { routesEnum } from '../../constants/routesEnum';
+import { logout } from '../../features/user/userSlice';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import './menu.scss';
 
-const Menu = props => {
+const Menu = ({ modalHandler }) => {
+
+    const dispatch = useDispatch();
+    const cerrarSesion = () => {
+        window.localStorage.removeItem('user');
+        window.localStorage.removeItem('todos');
+        dispatch(logout());
+    }
+
+
     return (
-        <div>Menu</div>
+        <div className="menu">
+            <Link className="menu__logo" to={routesEnum.HOME}>
+                <img src={logo} alt="Poincenot" />
+            </Link>
+
+            <div className="menu__login">
+                <button
+                    type='button'
+                    className="menu__login-resert"
+                    onClick={modalHandler}
+                >
+                    Reset
+                </button>
+                <Link
+                    className="menu__login-text"
+                    exact
+                    to={routesEnum.LOGIN}
+                    onClick={cerrarSesion}
+                >
+                    Cerrar Sesión
+                </Link>
+            </div>
+        </div >
     )
 }
 
-Menu.propTypes = {}
+Menu.propTypes = {
+    cerrarSesion: PropTypes.func,
+}
 
 export default Menu;
