7a7ee932dfa317056c3010c6264f0d3c24b783c7
fedecanesa <federicocanesa@gmail.com>
2022-07-27 13:11:35 -0300

    Page not found is added

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Layout } from '../../components';
+import { Link } from 'react-router-dom';
+import { notFound } from '../../utils/images';
 import PropTypes from 'prop-types';
-import './notFound.scss';
+import './notfound.scss';
 
-const NotFound = props => {
+const NotFound = () => {
     return (
-        <div>NotFound</div>
+
+        <div className="error__container">
+            <div className="error__box">
+                <div className="error__message">
+                    <div className="error__title">
+                        <h1>
+                            Opss... Pagina no encontrada!
+                        </h1>
+                    </div>
+                    <div className="error__button">
+                        <Link
+                            to={{
+                                pathname: '/'
+                            }}>
+                            Volver al inicio
+                        </Link>
+                    </div>
+                </div>
+                <div className="error__image">
+                    <img src={notFound} alt="Página no encontrada" />
+                </div>
+            </div>
+        </div>
+
     )
 }
 
--- src/utils/images.js
+++ src/utils/images.js
@@ -1,7 +1,9 @@
 import logo from '../assets/pcnt-logo.png';
 import add from '../assets/Add.png';
+import notFound from '../assets/not-found.png';
 
 export {
     logo,
-    add
+    add,
+    notFound
 }
