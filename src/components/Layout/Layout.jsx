e8ed36ee75a8be16dd22f2ccfb5783846b4757c0
fedecanesa <federicocanesa@gmail.com>
2022-07-27 12:29:54 -0300

    Props are added and styles are modified

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Menu } from '../'
 import './layout.scss';
 
-const Layout = props => {
+const Layout = ({ children, modalHandler  }) => {
     return (
-        <div>Layout</div>
+        <div className="layout">
+            <Menu modalHandler={modalHandler}/>
+            <main className="layout__container">
+                {children}
+                {/*   <Footer /> */}
+            </main>
+        </div>
     )
 }
 
-Layout.propTypes = {}
+Layout.propTypes = {
+    children: PropTypes.elementType
+}
 
 export default Layout;
