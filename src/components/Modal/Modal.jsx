c52cd606e10f2d06e29b1681c0016b55be8cbd08
fedecanesa <federicocanesa@gmail.com>
2022-07-27 12:41:34 -0300

    The modal component is created, the functionality and styles are made.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,12 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './modal.scss';
 
-const Modal = props => {
+const Modal = ({ modalHandler, resetList }) => {
     return (
-        <div>Modal</div>
+        <div className='overlay'>
+            <div className='Modal'>
+                <div className='Modal__header'>
+                    <h3>Empezar una nueva lista</h3>
+                </div>
+                <form className='Modal__body' onSubmit={resetList}>
+
+                    <section className='Modal__body--paragraph'>
+                        <p>
+                            Cuando comenzás una nueva lista, tu lista existente se elimina.
+                            ¿Estás seguro que querés empezar una nueva lista?
+                        </p>
+                    </section>
+                    <section className='Modal__buttonContainer'>
+                        <button
+                            className='button button__cancel'
+                            onClick={() => modalHandler()}
+                        >
+                            Cancelar
+                        </button>
+                        <button
+                            className='button button__ok'
+                            type='submit'
+                        >
+                            Nueva lista
+                        </button>
+                    </section>
+                </form>
+            </div>
+
+        </div>
     )
 }
 
-Modal.propTypes = {}
+Modal.propTypes = {
+    modalHandler: PropTypes.func,
+    resetList: PropTypes.func
+}
 
 export default Modal
